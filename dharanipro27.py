b78bfaa0148d7ed0f1b9592e63610111c26a1882
dharanianbu <52280951+dharanianbu@users.noreply.github.com>
2019-08-20 15:34:32 +0530

    Create dharanipro27.py

---
+++ dharanipro27.py
@@ -0,0 +1,14 @@
+#d
+s=input()
+n=len(s)
+
+def check_palindrome(s,n):
+    if s==s[-1:-n-1:-1]:
+        n-=1
+        s=s[0:n]
+        check_palindrome(s,n)
+    else:
+        print(s)
+
+
+check_palindrome(s,n)    
